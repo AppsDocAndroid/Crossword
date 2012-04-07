235481a2309d1d05a9b2821f4bcafc133c08642b
Alexis Lauper <alexis.lauper@gmail.com>
2012-04-07 18:34:00 +0200

    add game activity

---
+++ src/com/crossword/activity/FeedbackActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011 Alexis Lauper <alexis.lauper@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.crossword.activity;
+
+import com.crossword.Crossword;
+import com.crossword.R;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class FeedbackActivity extends CrosswordParentActivity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.feedback);
+	    
+	    WebView webView = (WebView)findViewById(R.id.webView);
+	    webView.loadUrl(Crossword.FEEDBACK_URL);
+	}
+	
+}
