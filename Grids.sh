965331cb566fa754823c5bd2dd17ba5005cd7a62
Alexis Lauper <alexis.lauper@gmail.com>
2012-04-12 00:05:30 +0200

    grids

--- Grids.sh
+++ Grids.sh
@@ -3,6 +3,7 @@
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" > Grids.xml
 echo "<gridlist>" >> Grids.xml
 for file in Grids/*.xml ; do
-	echo "<grid>$file</grid>" >> Grids.xml
+	name=`echo $file | cut -d '/' -f 2`
+	echo "<grid>$name</grid>" >> Grids.xml
 done
 echo "</gridlist>" >> Grids.xml
